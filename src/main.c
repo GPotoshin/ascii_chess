718059d31aada031f812aa6404223316cdd2649c
George Potoshin <george.potoshin@gmail.com>
2023-11-29 20:21:57 +0100

    drawing figures

--- src/main.c
+++ src/main.c
@@ -6,9 +6,6 @@
 
 #include <slog/slog.h>
 
-#define BLACK_PAIR 1
-#define WHITE_PAIR 2
-
 bool quite = false;
 
 typedef char figure[21];
@@ -18,6 +15,16 @@ typedef struct {
 	int x, y;
 	figure *fp;
 } chessobj;
+
+enum color_pairs {
+	WHITE_ON_BLACK,
+	BLACK_ON_WHITE,
+	BLUE_ON_WHITE,
+	BLUE_ON_BLACK,
+	RED_ON_WHITE,
+	RED_ON_BLACK
+};
+
 
 enum figures {
 	PAWN,
@@ -136,12 +143,41 @@ chessobj createobj (char const *n) {
 	return retval;
 }
 
-void mvaddfig (char *n) {
-   chessobj obj = createobj (n);
+void mvaddfig (char *n, int color) {
+	chessobj obj = createobj (n);
 
-   for (int i = 0; i < sizeof(figure); i++) {
-	   mvaddch (obj.y + i/7, obj.x + i%7, (*obj.fp)[i]);
-   }
+	if ((obj.x + obj.y)%2 == 0) {
+		if (COLOR_RED == color) {
+			attron(COLOR_PAIR(RED_ON_BLACK));
+		} else if (COLOR_BLUE == color) {
+			attron(COLOR_PAIR(BLUE_ON_BLACK));
+		}
+	} else {
+		if (COLOR_RED == color) {
+			attron(COLOR_PAIR(RED_ON_WHITE));
+		} else if (COLOR_BLUE == color) {
+			attron(COLOR_PAIR(BLUE_ON_WHITE));
+		}
+	}
+
+	for (int i = 0; i < sizeof(figure); i++) {
+		mvaddch (4*(7-obj.y) + i/7 + 2, obj.x*8 + i%7 + 2, (*obj.fp)[i]);
+	}
+
+	if ((obj.x + obj.y)%2 == 0) {
+		if (COLOR_RED == color) {
+			attroff(COLOR_PAIR(RED_ON_BLACK));
+		} else if (COLOR_BLUE == color) {
+			attroff(COLOR_PAIR(BLUE_ON_BLACK));
+		}
+	} else {
+		if (COLOR_RED == color) {
+			attroff(COLOR_PAIR(RED_ON_WHITE));
+		} else if (COLOR_BLUE == color) {
+			attroff(COLOR_PAIR(BLUE_ON_WHITE));
+		}
+	}
+
 }
 
 int init () {
@@ -153,8 +189,12 @@ int init () {
 	if (!has_colors ()) goto _bailout;
 
 	start_color();
-	init_pair (WHITE_PAIR, COLOR_BLACK, COLOR_WHITE);
-	init_pair (BLACK_PAIR, COLOR_WHITE, COLOR_BLACK);
+	init_pair (BLUE_ON_WHITE, COLOR_BLUE, COLOR_WHITE);
+	init_pair (BLUE_ON_BLACK, COLOR_BLUE, COLOR_BLACK);
+	init_pair (WHITE_ON_BLACK, COLOR_WHITE, COLOR_BLACK);
+	init_pair (BLACK_ON_WHITE, COLOR_BLACK, COLOR_WHITE);
+	init_pair (RED_ON_BLACK, COLOR_RED, COLOR_BLACK);
+	init_pair (RED_ON_WHITE, COLOR_RED, COLOR_WHITE);
 
 
 	for (int i = 0; i < 8; i++) {
@@ -169,34 +209,76 @@ int init () {
 			mvaddch (i, 1, '|');
 			if ((i-1)%8 < 5) {
 				for (int j = 0; j < 4; j++) {
-					attron (COLOR_PAIR(WHITE_PAIR));
+					attron (COLOR_PAIR(BLACK_ON_WHITE));
 					mvprintw (i, 2+j*16, "       ");
-					attroff (COLOR_PAIR(WHITE_PAIR));
+					attroff (COLOR_PAIR(BLACK_ON_WHITE));
 					mvaddch (i, 9+j*16, '|');
-					attron (COLOR_PAIR(BLACK_PAIR));
+					attron (COLOR_PAIR(WHITE_ON_BLACK));
 					mvprintw (i, 10+j*16, "       ");
-					attroff (COLOR_PAIR(BLACK_PAIR));
+					attroff (COLOR_PAIR(WHITE_ON_BLACK));
 					mvaddch (i, 17+j*16, '|');
 				}
 			}
 			else {
 				for (int j = 0; j < 4; j++) {
-					attron (COLOR_PAIR(BLACK_PAIR));
+					attron (COLOR_PAIR(WHITE_ON_BLACK));
 					mvprintw (i, 2+j*16, "       ");
-					attroff (COLOR_PAIR(BLACK_PAIR));
+					attroff (COLOR_PAIR(WHITE_ON_BLACK));
 					mvaddch (i, 9+j*16, '|');
-					attron (COLOR_PAIR(WHITE_PAIR));
+					attron (COLOR_PAIR(BLACK_ON_WHITE));
 					mvprintw (i, 10+j*16, "       ");
-					attroff (COLOR_PAIR(WHITE_PAIR));
+					attroff (COLOR_PAIR(BLACK_ON_WHITE));
 					mvaddch (i, 17+j*16, '|');
 				}
 			}
 		}
 	}
 
+	mvprintw	 (1, 72, "+-----------------------------+");
+	for (int i = 2; i < 33; i++) {
+		mvprintw (i, 72, "|                             |");
+	}
+	mvprintw	(33, 72, "+-----------------------------+");
+
+	mvaddch (32, 74, '>');
+
 	// putting chess figures on board
 
-	mvaddfig ("Re4");
+	mvaddfig ("a7", COLOR_BLUE);
+	mvaddfig ("b7", COLOR_BLUE);
+	mvaddfig ("c7", COLOR_BLUE);
+	mvaddfig ("d7", COLOR_BLUE);
+	mvaddfig ("e7", COLOR_BLUE);
+	mvaddfig ("f7", COLOR_BLUE);
+	mvaddfig ("g7", COLOR_BLUE);
+	mvaddfig ("h7", COLOR_BLUE);
+
+	mvaddfig ("Ra8", COLOR_BLUE);
+	mvaddfig ("Nb8", COLOR_BLUE);
+	mvaddfig ("Bc8", COLOR_BLUE);
+	mvaddfig ("Qd8", COLOR_BLUE);
+	mvaddfig ("Ke8", COLOR_BLUE);
+	mvaddfig ("Bf8", COLOR_BLUE);
+	mvaddfig ("Ng8", COLOR_BLUE);
+	mvaddfig ("Rh8", COLOR_BLUE);
+
+	mvaddfig ("a2", COLOR_RED);
+	mvaddfig ("b2", COLOR_RED);
+	mvaddfig ("c2", COLOR_RED);
+	mvaddfig ("d2", COLOR_RED);
+	mvaddfig ("e2", COLOR_RED);
+	mvaddfig ("f2", COLOR_RED);
+	mvaddfig ("g2", COLOR_RED);
+	mvaddfig ("h2", COLOR_RED);
+
+	mvaddfig ("Ra1", COLOR_RED);
+	mvaddfig ("Nb1", COLOR_RED);
+	mvaddfig ("Bc1", COLOR_RED);
+	mvaddfig ("Qd1", COLOR_RED);
+	mvaddfig ("Ke1", COLOR_RED);
+	mvaddfig ("Bf1", COLOR_RED);
+	mvaddfig ("Ng1", COLOR_RED);
+	mvaddfig ("Rh1", COLOR_RED);
 
 	return 0;
 _bailout:
